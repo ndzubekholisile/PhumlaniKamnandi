fa7c0630d261c8edf2915ac6ee5690f0262cf833
kholisile ndzube <ndzubekholisile@gmail.com>
2025-09-30 10:10:38 +0200

    Insert and Update Database operation Fixed. Delete Functionality left to be implemented

--- PhumlaniKamnandi/Data/DB.cs
+++ PhumlaniKamnandi/Data/DB.cs
@@ -6,16 +6,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PhumlaniKamnandi.Properties;//***needs to be added to be able to use the Settings property
 using System.Windows.Forms;
-using WaiteringSystem.Properties;//***needs to be added to be able to use the Settings property
-
-namespace WaiteringSystem.Data
+namespace PhumlaniKamnandi.Data
 {
     public class DB
     {
         #region Variable declaration
         //***Once the database is created you can find the correct connection string by using the Settings.Default object to select the correct connection string
-        private string strConn = Settings.Default.EmployeeDatabaseConnectionString;
+        private string strConn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\ndzub\\OneDrive\\Documents\\GitHub\\PhumlaniKamnandi\\PhumlaniKamnandi\\hotelDB.mdf;Integrated Security=True";
         protected SqlConnection cnMain;
         protected DataSet dsMain;
         protected SqlDataAdapter daMain;
@@ -37,7 +36,7 @@ namespace WaiteringSystem.Data
             }
             catch (SystemException e)
             {
-                System.Windows.Forms.MessageBox.Show(e.Message, "Error");
+                MessageBox.Show(e.Message, "Error");
                 return;
             }
         }
@@ -45,6 +44,11 @@ namespace WaiteringSystem.Data
         #endregion
 
         #region Update the DateSet
+        /// <summary>
+        /// Loads data from the database into a specified table in dataset.
+        /// </summary>
+        /// <param name="aSQLstring">SQL query string to select data.</param>
+        /// <param name="aTable">The dataset table name to fill.</param>
         public void FillDataSet(string aSQLstring, string aTable)
         {
             //fills dataset fresh from the db for a specific table and with a specific Query
@@ -52,7 +56,8 @@ namespace WaiteringSystem.Data
             {
                 daMain = new SqlDataAdapter(aSQLstring, cnMain);
                 cnMain.Open();
-                //dsMain.Clear();
+                if (dsMain.Tables[aTable]!= null)
+                    dsMain.Tables[aTable].Clear();
                 daMain.Fill(dsMain, aTable);
                 cnMain.Close();
             }
@@ -65,6 +70,12 @@ namespace WaiteringSystem.Data
         #endregion
 
         #region Update the data source 
+        /// <summary>
+        /// Updates the database with changes made in the dataset.
+        /// </summary>
+        /// <param name="sqlLocal">SQL query used to refresh the dataset after update.</param>
+        /// <param name="table">The name of the table to update.</param>
+        /// <returns>True if update succeeds, false otherwise.</returns>
         protected bool UpdateDataSource(string sqlLocal, string table)
         {
             bool success;
