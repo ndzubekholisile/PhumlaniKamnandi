bc70121c42f170a3f8316ceb965ea92cc3befca1
TBBGit <neochabedi07@gmail.com>
2025-10-01 21:13:57 +0200

    Added Controllers. Awaiting unit testing.

---
+++ PhumlaniKamnandi/Business/RoomAccountController.cs
@@ -0,0 +1,98 @@
+﻿// ========== Room Account Controller ==========
+using System;
+
+namespace PhumlaKamnandi.Business
+{
+    public class RoomAccountController
+    {
+        #region Constructor
+        public RoomAccountController()
+        {
+            hotelDB = new HotelDB();
+            roomaccounts = hotelDB.AllRoomAccounts;
+        }
+        #endregion
+
+        #region Data Members
+        HotelDB hotelDB;
+        Collection<RoomAccount> roomaccounts;
+        #endregion
+
+        #region Properties
+        public Collection<RoomAccount> AllRoomAccounts
+        {
+            get
+            {
+                return roomaccounts;
+            }
+        }
+        #endregion
+
+        #region Database Communication
+        public void DataMaintenance(RoomAccount aRoomAccount, DB.DBOperation operation)
+        {
+            int index = 0;
+            hotelDB.DataSetChange(aRoomAccount, operation);
+
+            switch (operation)
+            {
+                case DB.DBOperation.Add:
+                    roomaccounts.Add(aRoomAccount);
+                    break;
+                case DB.DBOperation.Edit:
+                    index = FindIndex(aRoomAccount);
+                    roomaccounts[index] = aRoomAccount;
+                    break;
+                case DB.DBOperation.Delete:
+                    index = FindIndex(aRoomAccount);
+                    roomaccounts.RemoveAt(index);
+                    break;
+            }
+
+        }
+
+        public bool FinalizeChanges(RoomAccount roomaccount)
+        {
+            return hotelDB.UpdateDataSource(roomaccount);
+        }
+        #endregion
+
+        #region Find Methods
+        public RoomAccount Find(string ID)
+        {
+            int index = 0;
+            bool found = (roomaccounts[index].AccountID == ID);
+            int count = AllRoomAccounts.Count;
+
+            while (!(found) && (index < roomaccounts.Count - 1))
+            {
+                index++;
+                found = (roomaccounts[index].AccountID == ID);
+            }
+
+
+            return AllRoomAccounts[index];
+        }
+
+        private int FindIndex(RoomAccount aRoomAccount)
+        {
+            int counter = 0;
+            bool found = false;
+            found = (aRoomAccount.AccountID == roomaccounts[counter].AccountID);
+            while (!found && counter <= roomaccounts.Count)
+            {
+                counter++;
+                found = (aRoomAccount.AccountID == roomaccounts[counter].AccountID);
+            }
+            if (found)
+            {
+                return counter;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        #endregion
+    }
+}
