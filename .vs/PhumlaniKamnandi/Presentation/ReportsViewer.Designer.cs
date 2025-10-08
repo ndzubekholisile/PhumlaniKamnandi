d0a1670b7e1660e4cb07c3932b085f5c550d9d44
Caleb Brown <BRWCAL007@myuct.ac.za>
2025-10-08 18:25:26 +0200

    Fixed error in ReportViewer form

--- .vs/PhumlaniKamnandi/Presentation/ReportsViewer.Designer.cs
+++ .vs/PhumlaniKamnandi/Presentation/ReportsViewer.Designer.cs
@@ -31,6 +31,7 @@ namespace PhumlaniKamnandi.Presentation
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.pnlMain = new System.Windows.Forms.Panel();
             this.btnExport = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
@@ -39,8 +40,8 @@ namespace PhumlaniKamnandi.Presentation
             this.lblReportType = new System.Windows.Forms.Label();
             this.lblReportTitle = new System.Windows.Forms.Label();
             this.pnlMain.SuspendLayout();
-            this.pnlHeader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).BeginInit();
+            this.pnlHeader.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlMain
@@ -53,7 +54,7 @@ namespace PhumlaniKamnandi.Presentation
             this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlMain.Location = new System.Drawing.Point(0, 0);
             this.pnlMain.Name = "pnlMain";
-            this.pnlMain.Size = new System.Drawing.Size(900, 600);
+            this.pnlMain.Size = new System.Drawing.Size(1029, 640);
             this.pnlMain.TabIndex = 0;
             // 
             // btnExport
@@ -61,11 +62,11 @@ namespace PhumlaniKamnandi.Presentation
             this.btnExport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(130)))), ((int)(((byte)(246)))));
             this.btnExport.FlatAppearance.BorderSize = 0;
             this.btnExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExport.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExport.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.btnExport.ForeColor = System.Drawing.Color.White;
-            this.btnExport.Location = new System.Drawing.Point(720, 530);
+            this.btnExport.Location = new System.Drawing.Point(823, 565);
             this.btnExport.Name = "btnExport";
-            this.btnExport.Size = new System.Drawing.Size(120, 40);
+            this.btnExport.Size = new System.Drawing.Size(137, 43);
             this.btnExport.TabIndex = 3;
             this.btnExport.Text = "Export to CSV";
             this.btnExport.UseVisualStyleBackColor = false;
@@ -76,11 +77,11 @@ namespace PhumlaniKamnandi.Presentation
             this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(163)))), ((int)(((byte)(175)))));
             this.btnClose.FlatAppearance.BorderSize = 0;
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.btnClose.ForeColor = System.Drawing.Color.White;
-            this.btnClose.Location = new System.Drawing.Point(580, 530);
+            this.btnClose.Location = new System.Drawing.Point(663, 565);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(100, 40);
+            this.btnClose.Size = new System.Drawing.Size(114, 43);
             this.btnClose.TabIndex = 2;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = false;
@@ -92,32 +93,12 @@ namespace PhumlaniKamnandi.Presentation
             this.dgvReport.AllowUserToDeleteRows = false;
             this.dgvReport.AllowUserToResizeRows = false;
             this.dgvReport.BackgroundColor = System.Drawing.Color.White;
-            this.dgvReport.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.dgvReport.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-            this.dgvReport.ColumnHeadersDefaultCellStyle = new DataGridViewCellStyle()
-            {
-                Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter,
-                BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(130)))), ((int)(((byte)(246))))),
-                Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point),
-                ForeColor = System.Drawing.Color.White,
-                SelectionBackColor = System.Drawing.SystemColors.Highlight,
-                SelectionForeColor = System.Drawing.SystemColors.HighlightText,
-                WrapMode = System.Windows.Forms.DataGridViewTriState.True
-            };
+            this.dgvReport.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvReport.DefaultCellStyle = new DataGridViewCellStyle()
-            {
-                Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft,
-                BackColor = System.Drawing.Color.White,
-                Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point),
-                ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(65)))), ((int)(((byte)(81)))),
-                (int)System.Windows.Forms.DataGridViewTriState.True)
-            };
-            this.dgvReport.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(234)))), ((int)(((byte)(254)))));
-            this.dgvReport.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(58)))), ((int)(((byte)(138)))));
             this.dgvReport.EnableHeadersVisualStyles = false;
             this.dgvReport.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(232)))), ((int)(((byte)(240)))));
-            this.dgvReport.Location = new System.Drawing.Point(30, 120);
+            this.dgvReport.Location = new System.Drawing.Point(34, 128);
             this.dgvReport.MultiSelect = false;
             this.dgvReport.Name = "dgvReport";
             this.dgvReport.ReadOnly = true;
@@ -125,7 +106,7 @@ namespace PhumlaniKamnandi.Presentation
             this.dgvReport.RowHeadersWidth = 51;
             this.dgvReport.RowTemplate.Height = 29;
             this.dgvReport.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvReport.Size = new System.Drawing.Size(840, 380);
+            this.dgvReport.Size = new System.Drawing.Size(960, 405);
             this.dgvReport.TabIndex = 1;
             // 
             // pnlHeader
@@ -134,39 +115,39 @@ namespace PhumlaniKamnandi.Presentation
             this.pnlHeader.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pnlHeader.Controls.Add(this.lblReportType);
             this.pnlHeader.Controls.Add(this.lblReportTitle);
-            this.pnlHeader.Location = new System.Drawing.Point(30, 30);
+            this.pnlHeader.Location = new System.Drawing.Point(34, 32);
             this.pnlHeader.Name = "pnlHeader";
-            this.pnlHeader.Size = new System.Drawing.Size(840, 80);
+            this.pnlHeader.Size = new System.Drawing.Size(960, 85);
             this.pnlHeader.TabIndex = 0;
             // 
             // lblReportType
             // 
             this.lblReportType.AutoSize = true;
-            this.lblReportType.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblReportType.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
             this.lblReportType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(114)))), ((int)(((byte)(128)))));
-            this.lblReportType.Location = new System.Drawing.Point(20, 45);
+            this.lblReportType.Location = new System.Drawing.Point(15, 48);
             this.lblReportType.Name = "lblReportType";
-            this.lblReportType.Size = new System.Drawing.Size(100, 21);
+            this.lblReportType.Size = new System.Drawing.Size(128, 28);
             this.lblReportType.TabIndex = 1;
             this.lblReportType.Text = "Report Type";
             // 
             // lblReportTitle
             // 
             this.lblReportTitle.AutoSize = true;
-            this.lblReportTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblReportTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
             this.lblReportTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(128)))));
-            this.lblReportTitle.Location = new System.Drawing.Point(20, 15);
+            this.lblReportTitle.Location = new System.Drawing.Point(3, 11);
             this.lblReportTitle.Name = "lblReportTitle";
-            this.lblReportTitle.Size = new System.Drawing.Size(120, 30);
+            this.lblReportTitle.Size = new System.Drawing.Size(171, 37);
             this.lblReportTitle.TabIndex = 0;
             this.lblReportTitle.Text = "Report Title";
             // 
             // ReportsViewer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(900, 600);
+            this.ClientSize = new System.Drawing.Size(1029, 640);
             this.Controls.Add(this.pnlMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -175,9 +156,9 @@ namespace PhumlaniKamnandi.Presentation
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Report Viewer";
             this.pnlMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).EndInit();
             this.pnlHeader.ResumeLayout(false);
             this.pnlHeader.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).BeginInit();
             this.ResumeLayout(false);
 
         }
