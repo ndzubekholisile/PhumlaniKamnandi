f0956fe4f70734656df28e2e3b5edaa82ec4ca2d
Caleb Brown <BRWCAL007@myuct.ac.za>
2025-10-08 18:40:52 +0200

    This is the proper commit in the .vs file pathway. other commit was older

--- .vs/PhumlaniKamnandi/Data/sql_scripts/populate_tables.sql
+++ .vs/PhumlaniKamnandi/Data/sql_scripts/populate_tables.sql
@@ -1,2 +1,6 @@
 ﻿INSERT INTO Booker (num_of_people_expected)
 VALUES (3);
+
+-- This Will Insert The Employee credentials
+INSERT INTO Employee (name, role, username, password)
+VALUES ('Hotel Clerk', 'Clerk', 'Clerk001', 'PK007');
