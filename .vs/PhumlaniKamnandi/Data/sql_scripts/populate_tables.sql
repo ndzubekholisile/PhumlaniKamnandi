f8ad44d6c1299c8ad390d96101173f9ca19991ca
ndzubekholisile <68696727+ndzubekholisile@users.noreply.github.com>
2025-12-30 19:40:10 +0200

    Delete .vs directory

--- .vs/PhumlaniKamnandi/Data/sql_scripts/populate_tables.sql
+++
@@ -1,6 +0,0 @@
-﻿INSERT INTO Booker (num_of_people_expected)
-VALUES (3);
-
--- This Will Insert The Employee credentials
-INSERT INTO Employee (name, role, username, password)
-VALUES ('Hotel Clerk', 'Clerk', 'Clerk001', 'PK007');
--- .vs/PhumlaniKamnandi/SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].Room (
-    roomID INT PRIMARY KEY IDENTITY(1,1),
-    accountID INT,
-    isOccupied BIT DEFAULT 0,
-    suiteType VARCHAR(20) NOT NULL CHECK (suiteType IN ('standard','deluxe','executive','family','presidential')),
-    FOREIGN KEY (accountID) REFERENCES RoomAccount(accountID)
-);
